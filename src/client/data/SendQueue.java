7075b09b940148c716bbf8255e736cb1f326d5a3
Jibin Parayil Thomas <jibinpt@gmail.com>
2018-04-25 02:54:06 -0500

    Merge branch 'master' of https://github.umn.edu/KHADA004/xFS
    # Conflicts:
    #	src/client/data/SendQueue.java

--- src/client/data/SendQueue.java
+++ src/client/data/SendQueue.java
@@ -60,14 +60,19 @@ public class SendQueue {
     public static long getExpectedDownloadTime(int bandwidth, ClientDetails receiver, String filename) {
         int downloadTime = waitTime / getMaxSupportedConcurrentSend();
         try {
-
+            //If atleast one free thread is there, then there is no need to add the avg time
+            //for a thread to be free
+            if(maxConcurrentSend - onGoingSends.get()>0){
+                downloadTime = 0;
+            }
             int minBandwidth = Math.min(bandwidth, MyInformation.getBandwidth());
 
             ConfigManager configManager = ConfigManager.create();
             String filepath = FileHandler.getFilePathForFileName(filename);
             String src_dest = MyInformation.getMyInformation().getClientId() + "-" + receiver.getClientId();
             int latency = configManager.getIntegerValue(src_dest);
-            downloadTime += latency;
+            //Convert latency from ms to s
+            downloadTime += latency/1000;
             downloadTime += getFileSizeInMb(filepath) / minBandwidth;
         } catch (Exception exception) {
             System.out.println("Missing Latency data. Setting to a default.");
